1c20211f7328b6668e2b01bc5a056fca56ca5a07
Shuntaro Kawakami <skawakam@hotmail.com>
2025-07-10 21:01:41 -0400

    More updates

--- load_auto_sales_data.R
+++ load_auto_sales_data.R
@@ -99,6 +99,183 @@ Volvo_XC60_Format <- Volvo_XC60 |>   pivot_longer(
   ) |> mutate(Model = 'Volvo XC60', Country='Sweden', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
   group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
 
-All <- rbind(Chevrolet_Silverado_Format, Ford_F_Series_Format, Honda_Civic_Format, Hyundai_Tucson_Format, Kia_Sportage_Format, Mini_Cooper_Format, Mitsubishi_Outlander_Format, Nissan_Rogue_Format, Subaru_Forester_Format, Toyota_RAV4_Format, Volvo_XC60_Format, Volkswagen_Jetta_Format)
+All_by_model <- rbind(Chevrolet_Silverado_Format, Ford_F_Series_Format, Honda_Civic_Format, Hyundai_Tucson_Format, Kia_Sportage_Format, Mini_Cooper_Format, Mitsubishi_Outlander_Format, Nissan_Rogue_Format, Subaru_Forester_Format, Toyota_RAV4_Format, Volvo_XC60_Format, Volkswagen_Jetta_Format)
 
 
+Audi <- read_csv("data/Audi.csv")
+Audi_Format <- Audi |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Audi', Country='Germany', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+BMW <- read_csv("data/BMW.csv")
+BMW_Format <- BMW |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'BMW', Country='Germany', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Cadillac <- read_csv("data/Cadillac.csv")
+Cadillac_Format <- Cadillac |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Cadillac', Country='USA', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Chevrolet <- read_csv("data/Chevrolet.csv")
+Chevrolet_Format <- Chevrolet |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Chevrolet', Country='USA', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Chrysler <- read_csv("data/Chrysler.csv")
+Chrysler_Format <- Chrysler |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Chrysler', Country='USA', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+GMC <- read_csv("data/GMC.csv")
+GMC_Format <- GMC |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'GMC', Country='USA', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Honda <- read_csv("data/Honda.csv")
+Honda_Format <- Honda |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Honda', Country='Japan', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Hyundai <- read_csv("data/Hyundai.csv")
+Hyundai_Format <- Hyundai |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Hyundai', Country='Korea', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Jaguar <- read_csv("data/Jaguar.csv")
+Jaguar_Format <- Jaguar |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Jaguar', Country='UK', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Jeep <- read_csv("data/Jeep.csv")
+Jeep_Format <- Jeep |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Jeep', Country='USA', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Kia <- read_csv("data/Kia.csv")
+Kia_Format <- Kia |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Kia', Country='Korea', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Land_Rover <- read_csv("data/Land_Rover.csv")
+Land_Rover_Format <- Land_Rover |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Land Rover', Country='UK', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Mazda <- read_csv("data/Mazda.csv")
+Mazda_Format <- Mazda |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Mazda', Country='Japan', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Mercedes_Benz <- read_csv("data/Mercedes_Benz.csv")
+Mercedes_Benz_Format <- Mercedes_Benz |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Mercedes Benz', Country='Germany', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Mini <- read_csv("data/Mini.csv")
+Mini_Format <- Mini |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Mini', Country='UK', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Mitsubishi <- read_csv("data/Mitsubishi.csv")
+Mitsubishi_Format <- Mitsubishi |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Mitsubishi', Country='Japan', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Nissan <- read_csv("data/Nissan.csv")
+Nissan_Format <- Nissan |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Nissan', Country='Japan', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Subaru <- read_csv("data/Subaru.csv")
+Subaru_Format <- Subaru |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Subaru', Country='Japan', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Toyota <- read_csv("data/Toyota.csv")
+Toyota_Format <- Toyota |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Toyota', Country='Japan', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Volkswagen <- read_csv("data/Volkswagen.csv")
+Volkswagen_Format <- Volkswagen |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Volkswagen', Country='Germany', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Volvo <- read_csv("data/Volvo.csv")
+Volvo_Format <- Volvo |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Volvo', Country='Sweden', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+Ford <- read_csv("data/Ford.csv")
+Ford_Format <- Ford |>   pivot_longer(
+    cols = Jan:Dec,
+    names_to = "Month",
+    values_to = "Sale"
+  ) |> mutate(Model = 'Ford', Country='USA', Month=match(Month, month.abb), Year=as.integer(Year), Date = ceiling_date(as.Date(paste(Year, Month, "01", sep="-")), "month") - days(1)) |> arrange(Model, Country, Month, Year) |>
+  group_by(Model, Country, Month) |> mutate(Sale_prev_year = lag(Sale, 1), Ratio_From_Prev_Year = (Sale - Sale_prev_year ) / Sale_prev_year)
+
+All <- rbind(Audi_Format, BMW_Format, Cadillac_Format, Chevrolet_Format, Chrysler_Format, GMC_Format, Honda_Format, Hyundai_Format, Jeep_Format, Kia_Format, Mazda_Format, Mini_Format, Mitsubishi_Format, Nissan_Format, Subaru_Format, Toyota_Format, Volkswagen_Format, Volvo_Format, Ford_Format)
